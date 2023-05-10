87b21a0e1833ef99d78dfa306fa94776f743a1ab
Guilherme Camargo <gwcamargo20@gmail.com>
2023-05-10 10:40:09 -0300

    tipos primitivos JavaScript

---
+++ meu-estudo-javascript/tipos_primitivos.js
@@ -0,0 +1,11 @@
+let nome = "Guilherme Camargo";
+let idade = 18;
+let boolean = true;
+let nullo = null;
+let undefined;
+
+console.log(typeof nome, nome);
+console.log(typeof idade, idade);
+console.log(typeof boolean, boolean);
+console.log(typeof nullo, nullo);
+console.log(typeof undefined, undefined);
