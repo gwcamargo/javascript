3e8eccbd7a539762b0e362ea4c99ba370b109bf9
Guilherme Camargo <gwcamargo20@gmail.com>
2023-05-11 10:28:17 -0300

    forEach JavaScript

--- meu-estudo-javascript/for.js
+++ meu-estudo-javascript/for.js
@@ -1,3 +1,11 @@
-let lista = ["Caderno", "Lápis", "Borracha", "Caneta"];
+let pedidos = [
+    { id: 50, nome: 'Guilherme', alimento: 'Pizza', bebida: 'Suco de uva'},
+    { id: 65, nome: 'Sofia', alimento: 'Coxinha', bebida: 'Suco de limão'},
+    { id: 28, nome: 'João', alimento: 'X-Búrguer', bebida: 'Refrigerante'},
+    { id: 36, nome: 'Ana Laura', alimento: 'Pastel', bebida: 'Refrigerante'}
+];
 
-lista.forEach((lista) => console.log(lista));
+pedidos.forEach((pedidos, index) => {
+    let {nome, alimento, bebida} = pedidos;
+    console.log(`${index} - O Cliente ${nome} fez o pedido do alimento ${alimento}, e sua bebida é ${bebida}`);
+});
