f4311724c4173ca95534eae063fbc1992c856948
Guilherme Camargo <gwcamargo20@gmail.com>
2023-05-15 11:48:50 -0300

    testes com listas JavaScript

--- aulas-javascript/lista.js
+++ aulas-javascript/lista.js
@@ -1,5 +1,25 @@
+// Criando as listas
 let cidades = ["São Paulo", "Avaré", "Campinas", "Guarulhos", "Ourinhos"];
+let cidades2 = cidades.slice(0, 3);
 
-cidades.splice(2, 3);
+let meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho"];
+let nomes = ["Guilherme", "Sofia", "João", "Melissa"];
+let objeto = ["Cadeira", "Porta", "Armário", "Panela"];
 
+// Configurações das listas 
+cidades.push("Sorocaba", "Carapicuiba");
+cidades.shift("São Paulo");
+
+objeto.splice(2, 1)
+
+meses.pop("Junho");
+
+nomes.unshift("Leonardo", "Ana Laura");
+nomes.shift("Leonardo");
+
+// Imprimindo as listas
 console.log(cidades);
+console.log(meses);
+console.log(nomes);
+console.log(objeto);
+console.log(cidades2);
