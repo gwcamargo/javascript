37b36b96a98c18d1f292c09efe0aff84d23aed7d
Guilherme Camargo <gwcamargo20@gmail.com>
2023-06-09 15:42:40 -0300

    classes e objetos JS

---
+++ classes_objetos/index.js
@@ -0,0 +1,36 @@
+class Pessoa {
+    nome
+    idade
+    sexo
+    cidade
+}
+
+let pessoa1 = new Pessoa()
+let pessoa2 = new Pessoa()
+let pessoa3 = new Pessoa()
+let pessoa4 = new Pessoa()
+
+pessoa1.nome = 'Guilherme'
+pessoa1.idade = 18
+pessoa1.sexo = 'Masculino'
+pessoa1.cidade = 'Manduri'
+
+pessoa2.nome = 'Sofia'
+pessoa2.idade = 22
+pessoa2.sexo = 'Feminino'
+pessoa2.cidade = 'São Paulo'
+
+pessoa3.nome = 'João'
+pessoa3.idade = 17
+pessoa3.sexo = 'Masculino'
+pessoa3.cidade = 'Avaré'
+
+pessoa4.nome = 'Maria Vitória'
+pessoa4.idade = 19
+pessoa4.sexo = 'Feminino'
+pessoa4.cidade = 'Ourinhos'
+
+console.log(pessoa1)
+console.log(pessoa2)
+console.log(pessoa3)
+console.log(pessoa4)
