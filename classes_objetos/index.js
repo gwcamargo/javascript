da101c0d0064f2fd3e62ba00a9d03ffbe5508ca5
Guilherme Camargo <gwcamargo20@gmail.com>
2023-06-12 15:53:19 -0300

    metodos get e set

--- classes_objetos/index.js
+++ classes_objetos/index.js
@@ -1,26 +1,48 @@
 class Pessoa {
-    nome
-    idade
-    sexo
+    _nome
+    _idade
+    _sexo
+    _cidade
+    _peso
+    _altura
+    _imc
 
-    cidade
-    peso
-    altura
-
-    imc
-
-    constructor (nome, idade, sexo, cidade, peso, altura, imc) {
-        this.nome = nome
-        this.idade = idade
-        this.sexo = sexo
-        this.cidade = cidade
-        this.peso = peso
-        this.altura = altura
-        this.imc = this.peso / (this.altura * this.altura)
+    constructor (nome, idade, sexo, cidade, peso, altura) {
+        this._nome = nome
+        this._idade = idade
+        this._sexo = sexo
+        this._cidade = cidade
+        this._peso = peso
+        this._altura = altura
+        this._imc = this.peso / (this.altura * this.altura)
     }
 
     calcula_imc() {
         return(this.peso / (this.altura * this.altura)).toFixed(2)
+    }
+
+    get nome() {
+        return this._nome
+    }
+
+    get idade() {
+        return this._idade
+    }
+
+    get cidade() {
+        return this._cidade
+    }
+
+    set nome(novoNome) {
+        this._nome = novoNome
+    }
+
+    set idade(novaIdade) {
+        this._idade = novaIdade
+    }
+
+    set cidade(novaCidade) {
+        this._cidade = novaCidade
     }
 }
 
@@ -29,12 +51,14 @@ let pessoa2 = new Pessoa('Sofia', 22, 'Feminino', 'São Paulo', 50.9, 1.55)
 let pessoa3 = new Pessoa('João', 17, 'Masculino', 'Avaré', 65.4, 1.77)
 let pessoa4 = new Pessoa('Maria Vitória', 19, 'Feminino', 'Ourinhos', 55.6, 1.68)
 
-console.log(pessoa1)
-console.log(pessoa2)
-console.log(pessoa3)
-console.log(pessoa4)
+console.log("Nome: " + pessoa1.nome)
+console.log("Idade: " + pessoa1.idade)
+console.log("Cidade: " + pessoa1.cidade)
 
-console.log(pessoa1.calcula_imc())
-console.log(pessoa2.calcula_imc())
-console.log(pessoa3.calcula_imc())
-console.log(pessoa4.calcula_imc())
+pessoa1.nome = 'Guilherme Camargo'
+pessoa1.idade = 19
+pessoa1._cidade = 'São Paulo'
+
+console.log("Nome: " + pessoa1.nome)
+console.log("Idade: " + pessoa1.idade)
+console.log("Cidade: " + pessoa1.cidade)
