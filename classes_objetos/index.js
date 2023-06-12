9527e6e6825c18351043420072bfda6e4bacf6ec
Guilherme Camargo <gwcamargo20@gmail.com>
2023-06-12 13:32:26 -0300

    metodo especifico

--- classes_objetos/index.js
+++ classes_objetos/index.js
@@ -2,42 +2,39 @@ class Pessoa {
     nome
     idade
     sexo
-    cidade
 
-    constructor (nome, idade, sexo, cidade) {
+    cidade
+    peso
+    altura
+
+    imc
+
+    constructor (nome, idade, sexo, cidade, peso, altura, imc) {
         this.nome = nome
         this.idade = idade
         this.sexo = sexo
         this.cidade = cidade
+        this.peso = peso
+        this.altura = altura
+        this.imc = this.peso / (this.altura * this.altura)
+    }
+
+    calcula_imc() {
+        return(this.peso / (this.altura * this.altura)).toFixed(2)
     }
 }
 
-let pessoa1 = new Pessoa('Guilherme', 18, 'Masculino', 'Manduri')
-let pessoa2 = new Pessoa('Sofia', 22, 'Feminino', 'São Paulo')
-let pessoa3 = new Pessoa('João', 17, 'Masculino', 'Avaré')
-let pessoa4 = new Pessoa('Maria Vitória', 19, 'Feminino', 'Ourinhos')
-
-// pessoa1.nome = 'Guilherme'
-// pessoa1.idade = 18
-// pessoa1.sexo = 'Masculino'
-// pessoa1.cidade = 'Manduri'
-
-// pessoa2.nome = 'Sofia'
-// pessoa2.idade = 22
-// pessoa2.sexo = 'Feminino'
-// pessoa2.cidade = 'São Paulo'
-
-// pessoa3.nome = 'João'
-// pessoa3.idade = 17
-// pessoa3.sexo = 'Masculino'
-// pessoa3.cidade = 'Avaré'
-
-// pessoa4.nome = 'Maria Vitória'
-// pessoa4.idade = 19
-// pessoa4.sexo = 'Feminino'
-// pessoa4.cidade = 'Ourinhos'
+let pessoa1 = new Pessoa('Guilherme', 18, 'Masculino', 'Manduri', 71.5, 1.93)
+let pessoa2 = new Pessoa('Sofia', 22, 'Feminino', 'São Paulo', 50.9, 1.55)
+let pessoa3 = new Pessoa('João', 17, 'Masculino', 'Avaré', 65.4, 1.77)
+let pessoa4 = new Pessoa('Maria Vitória', 19, 'Feminino', 'Ourinhos', 55.6, 1.68)
 
 console.log(pessoa1)
 console.log(pessoa2)
 console.log(pessoa3)
 console.log(pessoa4)
+
+console.log(pessoa1.calcula_imc())
+console.log(pessoa2.calcula_imc())
+console.log(pessoa3.calcula_imc())
+console.log(pessoa4.calcula_imc())
