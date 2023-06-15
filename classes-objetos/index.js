7132f72b7be93562075027c9b0ae5beebc1fadfb
Guilherme Camargo <gwcamargo20@gmail.com>
2023-06-15 17:43:20 -0300

    estudos em andamento

--- classes-objetos/index.js
+++ classes-objetos/index.js
@@ -1,63 +1,8 @@
+import { Pessoa } from "./pessoa.js"
+
 // classes = moldes ou modelos de objetos
 // objetos = são abstrações ou representações de 'coisas' que existem no mundo
 // objeto = instância ou objeto do tipo | objeto é um tipo de dados
-
-class Pessoa {
-
-    // atributos = variáveis ou características
-    _nome
-    _idade
-    _sexo
-    _cidade
-    _peso
-    _altura
-    _imc
-    static totalPessoas = 0 // atributos estáticos (static) ou da class
-
-    // métodos = funções ou comportamentos
-    // método construtor
-    constructor (nome, idade, sexo, cidade, peso, altura) {
-        this._nome = nome
-        this._idade = idade
-        this._sexo = sexo
-        this._cidade = cidade
-        this._peso = peso
-        this._altura = altura
-        this._imc = this._peso / (this._altura * this._altura)
-        Pessoa.totalPessoas += 1  // contador de pessoas com incremento
-
-    }
-
-    calcula_imc() {
-        return (this._peso / (this._altura * this._altura)).toFixed(2)
-    }
-
-    // método get = pegar informações
-    get nome() {
-        return this._nome
-    }
-
-    get idade() {
-        return this._idade
-    }
-
-    get cidade() {
-        return this._cidade
-    }
-
-    // método set = editar, alterar, configurar informações
-    set nome(novoNome) {
-        this._nome = novoNome
-    }
-
-    set idade(novaIdade) {
-        this._idade = novaIdade
-    } 
-
-    set cidade(novaCidade) {
-        this._cidade = novaCidade
-    }
-}
 
 // criar novo objeto a partir da class, instanciar
 let pessoa1 = new Pessoa('Guilherme', 18, 'Masculino', 'Manduri', 71.0, 1.93)
@@ -67,6 +12,11 @@ let pessoa4 = new Pessoa('Isabela', 18, 'Feminino', 'Ourinhos', 50.8, 1.73)
 
 // exibindo na tela o número total de pessoas
 console.log(Pessoa.totalPessoas)
+
+console.log("Situação: " + pessoa1.classifica_imc())
+console.log("Situação: " + pessoa2.classifica_imc())
+console.log("Situação: " + pessoa3.classifica_imc())
+console.log("Situação: " + pessoa4.classifica_imc())
 
 // exibindo os resultados na tela (antes)
 console.log("Nome: " + pessoa1.nome)
---
+++ classes-objetos/pessoa.js
@@ -0,0 +1,81 @@
+export class Pessoa {
+
+    // atributos = variáveis ou características
+    _nome
+    _idade
+    _sexo
+    _cidade
+    _peso
+    _altura
+    _imc
+    static totalPessoas = 0 // atributos estáticos (static) ou da class
+
+    // métodos = funções ou comportamentos
+    // método construtor
+    constructor (nome, idade, sexo, cidade, peso, altura) {
+        this._nome = nome
+        this._idade = idade
+        this._sexo = sexo
+        this._cidade = cidade
+        this._peso = peso
+        this._altura = altura
+        this._imc = this._peso / (this._altura * this._altura)
+        Pessoa.totalPessoas += 1  // contador de pessoas com incremento
+
+    }
+
+    calcula_imc() {
+        return (this._peso / (this._altura * this._altura)).toFixed(2)
+    }
+
+    // Classificando o valor do imc
+    classifica_imc(valorImc) {
+        valorImc = (this._imc).toFixed(2)
+
+        let classificacao = ""
+        
+        if (valorImc < 18.5) {
+            classificacao = "Abaixo do peso"
+        } else if (valorImc <= 24.9) {
+            classificacao = "Peso normal"
+        } else if (valorImc <= 29.9) {
+            classificacao = "Sobrepeso"
+        } else if (valorImc <= 34.9) {
+            classificacao = "Obesidade grau I"
+        } else if (valorImc <= 39.9) {
+            classificacao = "Obesidade grau II"
+        } else if (valorImc >= 40) {
+            classificacao = "Obesidade grau III ou mórbida"
+        } else {
+            classificacao = "Peso Inválido"
+        }
+
+        return classificacao
+    } 
+
+    // método get = pegar informações
+    get nome() {
+        return this._nome
+    }
+
+    get idade() {
+        return this._idade
+    }
+
+    get cidade() {
+        return this._cidade
+    }
+
+    // método set = editar, alterar, configurar informações
+    set nome(novoNome) {
+        this._nome = novoNome
+    }
+
+    set idade(novaIdade) {
+        this._idade = novaIdade
+    } 
+
+    set cidade(novaCidade) {
+        this._cidade = novaCidade
+    }
+}
